1f7cf1743eb6498fb1d9590038330711616566a0
YangsonHung <2545594421@qq.com>
2025-03-13 13:26:05 +0800

    add

--- chapter07/cli-demo/src/components/插槽/App.vue
+++ chapter07/cli-demo/src/components/插槽/App.vue
@@ -32,22 +32,50 @@
         <div>2</div>
         <div>3</div>
     </NarBar>
+
+    <!-- 类似 children 函数渲染 props render -->
+    <ShowNames :names="names">
+        <template v-slot:default="slotProps">
+            <span>{{ slotProps.item }} - {{ slotProps.index }} - {{ slotProps.hyx }}</span>
+        </template>
+    </ShowNames>
+
+    <br>
+    <ShowNames :names="names2">
+        <template v-slot="my">
+            <span>{{ my.item }} - {{ my.index }} - {{ my.hyx }}</span>
+        </template>
+
+        <template v-slot:hyx>
+            <div>hyxxxxxxxx</div>
+        </template>
+    </ShowNames>
+
+    <br>
+    <ShowNames :names="names3" v-slot="my">
+        <span>{{ my.item }} - {{ my.index }} - {{ my.hyx }}</span>
+    </ShowNames>
 </template>
 
 <script>
 import MySlotCpn from './MySlotCpn.vue';
 import MyButton from './MyButton.vue';
 import NarBar from './NarBar.vue';
+import ShowNames from './ShowNames.vue';
 export default {
     components: {
         MySlotCpn,
         MyButton,
-        NarBar
+        NarBar,
+        ShowNames
     },
     data() {
         return {
             name: 'hyx',
-            test: 'test'
+            test: 'test',
+            names: ['jack', 'Bob', 'Candy'],
+            names2: ['jack2', 'Bob2', 'Candy2'],
+            names3: ['jack3', 'Bob3', 'Candy3']
         }
     }
 }
---
+++ chapter07/cli-demo/src/components/插槽/ShowNames.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="show-names">
+        <template v-for="(item, index) in names" :key="item">
+            <slot hyx="123" :item="item" :index="index"></slot>
+            <br>
+        </template>
+        <slot name="hyx"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        names: {
+            type: Array,
+            default: () => []
+        }
+    }
+}
+</script>
+
+<style scoped></style>
